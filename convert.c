3d24db6683f861ca9174f293ec90ea1036fc495c
Andrew Oakes <andrew.oakes@wsu.edu>
2018-10-09 23:21:59 -0700

    Added relevant comments to convert.c

--- convert.c
+++ convert.c
@@ -1,8 +1,7 @@
 /* FUNCTIONALITY
  *
  * Takes an array containing the parsed regular expression and converts it
- * into a DFA (Mean NFA :( ), which is then stored in the output file specified in fileIO
- * by default outputs to stdin.
+ * into a NFA, which is then printed out in the form of a transition table. 
  *
 */
 #include <stdio.h>
@@ -16,93 +15,53 @@ int main(int argc, char** argv){
                         // Default is the file regex.txt in this dir.
         setFileName(argv[1]);
     }
-    int max = 300;
+    int max = 300;      // The maximum length of regex accepted.
     char* reg = (char*)malloc(sizeof(char)*max);
     int res = 0;
     char c;
     DFA* p1,*p2;
-    DFAArray* st = DFAArrayInit(10); 
-    DFA* Final = NULL;
-    int skip = 0;
+    DFAArray* st = DFAArrayInit(10); // The "Stack" that will hold the DFAs
+    DFA* Final = NULL;               // The pointer to the completed DFA
     while(res != -1 ){
-        res = nextRegex(reg,max);
-        if(res != -1){
+        res = nextRegex(reg,max);    // Get the next regex from the file.
+        if(res != -1){               // Check if not end of file.
             for(int i  = 0;(c = reg[i])!= '\0'; i++){
                 if(isAlphabet(c)){
-                    DFAArrayAdd(st,DFAGen(c));
+                    DFAArrayAdd(st,DFAGen(c));// Create an NFA that accepts c.
                 }else if(isOp(c)){
                     if(c == '*'){
-                        p1 = DFAPop(st);
-                        DFAArrayAdd(st,DFAStar(p1));
+                        p1 = DFAPop(st);      // Get top NFA and apply star.
+                        DFAArrayAdd(st,DFAStar(p1));// Add the resulting NFA to the stack.
                     }else if(c == '|'){
                         p2 = DFAPop(st);
-                        p1 = DFAPop(st);
-                        DFAArrayAdd(st,DFAOr(p1,p2));
+                        p1 = DFAPop(st);      // apply Or operation on the top two NFAs
+                        DFAArrayAdd(st,DFAOr(p1,p2));// Add the resulting NFA to the stack.
                     }else if(c == '&'){
-                        p2 = DFAPop(st);
+                        p2 = DFAPop(st);      // apply AND operation to the top two NFAs
                         p1 = DFAPop(st);
-                        DFAArrayAdd(st,DFAAnd(p1,p2));
+                        DFAArrayAdd(st,DFAAnd(p1,p2));// Add the resulting NFA to the stack.
                     }else{
                         printf("Malformed Regex, exiting.\n");
                         return 0; 
                     }
                 }
             } 
+        // Pop the last DFA off the stack and print it out.
         Final = DFAPop(st);
         printDFA(Final);
         }
         if(Final != NULL){
             DFAFree(&Final);
         }
-        clearDFAArray(st); 
+        // Make sure the stack ptr is set to 0 each time.
+        if(st->used > 0){
+            printf("Malformed Regex, resulting table may be invalid.")
+            clearDFAArray(st);
+        } 
     }
+    // Free all the things and close the files.
     DFAArrayFree(&st);
     free(reg);
     closeFiles();
-        /*stack* st = stackInit();
-    push(st,'a');
-    push(st,'b');
-    printf("%c %c \n",pop(st),pop(st));
-
-    DFA* dfa = DFAinit(0,1); 
-    intArray* arr = intArrayInit(10);
-
-    for(int i = 1; i < 101; i++){
-        transitionArrayAdd(dfa->transitions,transitionInit('a',DFAinit(i,0)));
-        intArrayAdd(arr,i);
-    }
-
-    for(int i = 0; i<101; i++){
-        printf("Is %i in arr? %i \n",i,intIn(arr,i));
-    }
-
-    DFA* q0 = DFAinit(0,0);
-    DFA* q1 = DFAinit(1,0);
-    DFA* q2 = DFAinit(2,1);
-    
-    transitionArrayAdd(q0->transitions,transitionInit('a',q1));
-    transitionArrayAdd(q1->transitions,transitionInit('b',q2));
-    //transitionArrayAdd(q2->transitions,transitionInit('E',q0));
-
-    DFA* z0 = DFAinit(0,0);
-    DFA* z1 = DFAinit(1,0);
-    DFA* z2 = DFAinit(2,1);
-    z0 = DFAStar(z0);
-    q0 = DFAStar(q0);
-    transitionArrayAdd(z0->transitions,transitionInit('b',z1));
-    transitionArrayAdd(z1->transitions,transitionInit('a',z2));
-    //transitionArrayAdd(z2->transitions,transitionInit('E',z0));
-    printf("Entering the and\n");
-    q0 = DFAOr(q0,z0);
-    q0 = DFAStar(q0);
-    printDFA(q0);
-    printf("Exiting the and\n"); 
-    printf("%i\n",dfa->transitions->used);
-    intArrayFree(&arr);
-    DFAFree(&dfa);
-    DFAFree(&q0);
-    free(st);
-    free(reg);
-    closeFiles();
-    return 0;*/
+        
 }
